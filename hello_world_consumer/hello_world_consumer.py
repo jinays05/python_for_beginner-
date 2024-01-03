f6d1eb2cd1270bc78f21806fa2d1d06a222d494d
jinay <jinays05@gmail.com>
2024-01-02 22:04:52 -0800

    this is a hello world in python

---
+++ hello_world_consumer/hello_world_consumer.py
@@ -0,0 +1,4 @@
+from hello_world_provider.hello_world_producer import hello_world_fn
+print("test-1")
+if __name__ == "__main__":
+    hello_world_fn()
---
+++ hello_world_provider/hello_world_producer.py
@@ -0,0 +1,10 @@
+print(__name__)
+
+
+def hello_world_fn():
+    print(__name__)
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    hello_world_fn()
